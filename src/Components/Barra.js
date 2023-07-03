fc299d0970900096a4acdf87fc657260f49f1ae4
markel.orallo.next <markel.orallo.next@bbva.com>
2023-07-03 04:04:46 +0200

    Added

--- src/Components/Barra.js
+++ src/Components/Barra.js
@@ -113,7 +113,7 @@ export function Barra(props) {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            Autos Factory
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
